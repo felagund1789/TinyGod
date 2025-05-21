4c82bd58db509d750a9de52f4920cbde639df905
Dimitris Stavroulakis <jimstav@gmail.com>
2025-05-22 01:36:31 +0300

    Add initial version of the Main Menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button quitButton;
+    [SerializeField] private GameObject planet;
+    [SerializeField] private float rotationSpeed = 10f;
+
+    void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    void Update()
+    {
+        // Slowly rotate the planet for visual effect
+        planet.transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+
+    private void StartGame()
+    {
+        SceneTransition.LoadScene("SampleScene");
+    }
+
+    private void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
