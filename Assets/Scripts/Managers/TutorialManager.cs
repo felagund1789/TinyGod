cb4039acb2730f3d3806829d8c4d362e39c11590
Konstantinos K <raistlindragonl@gmail.com>
2025-05-25 01:27:05 +0300

    Add more detailed tutorial messages. Show important messages at the beginning.

--- Assets/Scripts/Managers/TutorialManager.cs
+++ Assets/Scripts/Managers/TutorialManager.cs
@@ -9,19 +9,16 @@ namespace Managers
     public class TutorialManager : MonoBehaviour
     {
         [Header("UI Elements")]
-        [SerializeField] private GameObject tutorialBackgroundPanel;
-        [SerializeField] private RectTransform tutorialMessagePanel;
+        [SerializeField] private GameObject welcomeMessagePanel;
+        [SerializeField] private GameObject powersExplanationPanel;
+        [SerializeField] private GameObject tutorialMessagePanel;
         [SerializeField] private TextMeshProUGUI tutorialMessageText;
-        private bool _faithMessageShown = false;
-        private bool _rainMessageShown = false;
         private bool _npcReproduceMessageShown = false;
         private bool _foodProducedMessageShown = false;
 
         private void Awake()
         {
             // Subscribe to events
-            Bus<FaithUsedEvent>.OnEvent += OnFaithUsedEvent;
-            Bus<RainEvent>.OnEvent += OnRainEvent;
             Bus<NPCReproduceEvent>.OnEvent += OnNPCReproduceEvent;
             Bus<FoodProducedEvent>.OnEvent += OnFoodProducedEvent;
         }
@@ -29,7 +26,8 @@ namespace Managers
         void Start()
         {
             Invoke(nameof(ShowWelcomeMessage), 1f);
-            Invoke(nameof(ShowRightClickToRotateMessage), 2f);
+            Invoke(nameof(ShowPowersExplainer), 1.1f);
+            Invoke(nameof(ShowRightClickToRotateMessage), 1.2f);
         }
 
         private void Update()
@@ -42,77 +40,68 @@ namespace Managers
 
         private void OnDestroy()
         {
-            Bus<FaithUsedEvent>.OnEvent -= OnFaithUsedEvent;
-            Bus<RainEvent>.OnEvent -= OnRainEvent;
             Bus<NPCReproduceEvent>.OnEvent -= OnNPCReproduceEvent;
             Bus<FoodProducedEvent>.OnEvent -= OnFoodProducedEvent;
-        }
-
-        private void OnFaithUsedEvent(FaithUsedEvent args)
-        {
-            if (_faithMessageShown) return;
-            _faithMessageShown = true;
-            ShowMessage(@"Great! You have used some of your faith to use your powers.
-
-You can switch between powers by pressing 1 for the Rain and 2 for the Fireball, or by clicking on the icons in the UI.");
-        }
-
-        private void OnRainEvent(RainEvent args)
-        {
-            if (_rainMessageShown) return;
-            _rainMessageShown = true;
-            ShowMessage(@"Rain is a powerful tool! 
-
-If one or more farms are within the radius of the rain, they will be watered and produce food to feed your followers. 
-
-Otherwise, a small forest will be created where the rain fell.");
         }
 
         private void OnNPCReproduceEvent(NPCReproduceEvent args)
         {
             if (_npcReproduceMessageShown) return;
             _npcReproduceMessageShown = true;
-            ShowMessage(@"Every sometime, your followers will reproduce and create new followers.
-
-More followers means more faith is generated. But followers consume food, so make sure you have enough food to feed them all!");
+            ShowMessage("Every sometime, your followers will reproduce and create new followers. " +
+                        "More followers means more faith is generated. But followers consume food, " +
+                        "so make sure you have enough food to feed them all!");
         }
 
         private void OnFoodProducedEvent(FoodProducedEvent args)
         {
             if (_foodProducedMessageShown) return;
             _foodProducedMessageShown = true;
-            ShowMessage(@"Your farms produce food which your followers will consume.
-
-If there is not enough food, they will become unhappy and their faith in you will dwindle.");
+            ShowMessage("Your farms produce food which your followers will consume. " +
+                        "If there is not enough food, they will become unhappy and their faith in you will dwindle.");
 
             // Show the message for 5 seconds
             Invoke(nameof(CloseTutorial), 5f);
         }
 
-        private void ShowWelcomeMessage() =>
-            ShowMessage(@"Welcome to Tiny God!
+        private void ShowWelcomeMessage()
+        {
+            Time.timeScale = 0;
+            welcomeMessagePanel.SetActive(true);
+        }
 
-You are a god who watches over a tiny planet with tiny followers.
-Use your powers to help them grow and thrive.
-
-Press T to quit this tutorial at any time.");
+        private void ShowPowersExplainer()
+        {
+            Time.timeScale = 0;
+            powersExplanationPanel.SetActive(true);
+        }
 
         private void ShowRightClickToRotateMessage() =>
-            ShowMessage(@"You can right-click and move the mouse to rotate the planet.
-
-Left click on the planet to use the selected power.");
+            ShowMessage("You can right-click and move the mouse to rotate the planet. " +
+                        "Left click on the planet to use the selected power.");
 
         private void ShowMessage(string message)
         {
             Time.timeScale = 0;
             tutorialMessageText.SetText(message);
-            // tutorialMessagePanel.sizeDelta = tutorialMessageText.GetPreferredValues();
-            tutorialBackgroundPanel.SetActive(true);
+            tutorialMessagePanel.gameObject.SetActive(true);
+        }
+
+        public void CloseWelcomeMessage()
+        {
+            welcomeMessagePanel.SetActive(false);
+            Time.timeScale = 1;
+        }
+
+        public void ClosePowersExplainer()
+        {
+            powersExplanationPanel.SetActive(false);
+            Time.timeScale = 1;
         }
 
         public void CloseTutorial()
         {
-            tutorialBackgroundPanel.SetActive(false);
+            tutorialMessagePanel.gameObject.SetActive(false);
             Time.timeScale = 1;
         }
 
