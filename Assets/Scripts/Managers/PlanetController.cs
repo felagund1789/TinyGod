1e2591b26d20d077ef5815c831a18387712366c1
Konstantinos K <raistlindragonl@gmail.com>
2025-05-28 23:36:56 +0300

    Add touch support for mobile

--- Assets/Scripts/Managers/PlanetController.cs
+++ Assets/Scripts/Managers/PlanetController.cs
@@ -6,15 +6,23 @@ namespace Managers
     public class PlanetController : MonoBehaviour
     {
         [SerializeField] private float rotationSpeed = 360f;
+        public bool IsDragging { get; private set;  } = false;
 
         void Update()
         {
-            if (Mouse.current == null || !Mouse.current.rightButton.isPressed)
+            if (Mouse.current == null || (!Mouse.current.rightButton.isPressed && !Mouse.current.leftButton.isPressed))
+            {
+                IsDragging = false;
                 return;
+            }
 
             Vector2 delta = Mouse.current.delta.ReadValue();
-            transform.Rotate(Vector3.up, -delta.x * rotationSpeed * Time.deltaTime, Space.World);
-            transform.Rotate(Vector3.right, delta.y * rotationSpeed * Time.deltaTime, Space.World);
+            if (delta.sqrMagnitude > 0.01f) // Detect small movements as dragging
+            {
+                IsDragging = true;
+                transform.Rotate(Vector3.up, -delta.x * rotationSpeed * Time.deltaTime, Space.World);
+                transform.Rotate(Vector3.right, delta.y * rotationSpeed * Time.deltaTime, Space.World);
+            }
         }
     }
 }
